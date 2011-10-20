5084f2ed9b178bba6a00023836bf96f18b881011
cjohns <cjohns@middil.com>
2011-10-20 15:43:25 -0700

    added roles to side menu

--- www/application/views/dependencies/navigation_menu.php
+++ www/application/views/dependencies/navigation_menu.php
@@ -12,21 +12,30 @@
 					<li>
 						<a href="<? // =site_url("gale");?>">Gale</a>
 					</li>-->
+					<?php $this->load->library('UserAccess'); ?>
 					<li>
 						<a href="<?=site_url("customreport");?>">Custom Reports</a>
 					</li>
+					<?php if ($this->useraccess->HasRole(array('report admin','system admin'))): ?>
 					<li>
 						<a href="<?=site_url("reportbuilder");?>">Report Builder</a>
 					</li>
+					<?php endif; ?>
+					<?php if ($this->useraccess->HasRole(array('user admin','system admin'))): ?>
 					<li>
 						<a href="<?=site_url("useradmin");?>">Account Admin</a>
 					</li>
+					<?php endif; ?>
+					<?php if ($this->useraccess->HasRole(array('system admin'))): ?>
 					<li>
 						<a href="<?=site_url("sysadmin");?>">System Admin</a>
 					</li>
+					<?php endif; ?>
+					<?php if ($this->useraccess->HasRole(array('system admin','internal'))): ?>
 					<li>
 						<a href="<?=site_url("pglmsweeklies");?>">PGLMS Weekly Reports</a>
 					</li>
+					<?php endif; ?>
 					<!-- 
 						This runs via cron job, not necessary to have direct access<li>
 						<a href="_mil_enrollments.php">Biweekly MIL Enrollments</a>
