b3c54e2c85ff0634249ae0f7bdf11802bcf806f3
Clay Johns <clay.johns@moneydesktop.com>
2013-04-23 09:51:57 -0600

    genericified the title

--- www/application/views/dependencies/header.php
+++ www/application/views/dependencies/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
-		<title>MIL-BI - <?=$title?></title>
+		<title>Simple Report Runner - <?=$title?></title>
 		<? $this->load->view('dependencies/source_links'); ?>
 	</head>
 	<body>
@@ -13,11 +13,11 @@
 			<div id="logOutLink" style="position: relative;">
 				<span><a style="color:#FFF" href="<?=base_url()?>login/logout">log out</a></span>
 			</div>
-			<div class="clear"></div> <!-- clear floats -->	
+			<div class="clear"></div> <!-- clear floats -->
 			<?php
 				$this->load->view('dependencies/navigation_menu');
 			?>
-			
+
 			<div id="main">
 				<div id="formElements">
 					<div id="createReports" class="section">
